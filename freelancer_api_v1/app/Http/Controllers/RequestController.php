1a7a850d3217efd8b7f4374ed218b7f702e17e34
youngizere <Young.Izere@strathmore.edu>
2020-09-01 11:23:30 +0200

    Requests

--- freelancer_api_v1/app/Http/Controllers/RequestController.php
+++ freelancer_api_v1/app/Http/Controllers/RequestController.php
@@ -9,90 +9,9 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //Start-- Examples for illustration
 
-    // //create(one) [POST]
-    // public function newItem(Request $request){
-    //     $project = new Project;
-    //     $project->attr1 = /*request("attr1")*/ /*or*/ /*$request->input("attr1")*/ /*or*/ $request->attr1;
-    //     $project->save();
+    //----------------------------------------------------------------------------------------------------
 
-    //     //JSON below
-    //     return response()->json( ["message" => "new project created"], 201 );
-    // }
-    // //Route::post(projects/, "ProjectController@newItem")
-
-    // //show (one) [GET]
-    // public function showItem($id){
-    //     //$project = Project::find($id); //option 1
-        
-    //     if(Project::where("id", $id)->exists()){
-    //         $project = Project::where("id", $id)->get()->toJson(JSON_PRETTY_PRINT);
-    //         //JSON below
-    //         return response($project, 200);
-    //     }
-    //     else{
-    //         //JSON below
-    //         return response()->json( ["message" => "project not found"], 404 );
-    //     }
-
-    // }
-    // //Route::get(projects/{id}, "ProjectController@showItem")
-
-    // //delete (one) [DELETE]
-    // public function deleteItem($id){
-    //     //$project = Project::find($id);//option 1
-    //     //$project->delete();
-
-    //     if(Project::where("project_id", $id)->exists()){
-    //         $project = Project::find($id);
-    //         $project->delete();
-    //         //JSON below
-    //         return response()->json( ["message" => "projects deleted"], 200 );
-    //     }
-    //     else{
-    //         //JSON below
-    //         return response()->json( ["message" => "project not found"], 401 );
-    //     }
-        
-    // }
-    // //Route::delete(projects/{id}, ProjectController@deleteItem)
-
-    // //update(one) [PUT]
-    // public function updateItem(Request $request, $id){
-    //     //$project = Project::find($id);//option 1
-    //     //$project->attr1 = "12345";
-    //     //$project->save();
-
-    //     if(Project::where("project_id", $id)->exists()){
-    //         $project = Project::find($id);
-    //         $project->attr1 = is_null($request->name) ? $project->attr1 : $request->attr1;
-    //         $project->save();
-    //         //JSON below
-    //         return response()->json( ["message" => "projects updated successfully"], 200 );
-    //     }
-    //     else{
-    //         //JSON below
-    //         return response()->json( ["message" => "project not found"], 401 );
-    //     }
-        
-    // }
-    // //Route::put(projects/{id}, ProjectController@updateItem)
-
-    // //show(all) [GET]
-    // public function showAllprojects(){
-    //     $projects = Project::all(); //option 1
-    //     $projects = Project::get()->toJson(JSON_PRETTY_PRINT);
-
-    //     //JSON below
-    //     return response($projects, 200);
-    // }
-    // //Route:post(projects/, ProjectController@showAllprojects)
-
-    //End
-
-    //Functions below
-    //show many [GET]
     public function showProjectDetails($id){
         //get projects which belong to freelancer
         $projects = DB::table("projects")->where("appuser_freelancer_id", "=", $id)->get()->toArray();
@@ -119,11 +38,13 @@ class ProjectController extends Controller
 
         $projects2 = array();
         $projects2["data"] = $projects;
-        $projects2 = $projects;
+        //$projects2 = $projects;
 
         return response()->json( $projects2, 200 );
 
     }
+
+    //-------------------------------------------------------------------------------
 
     public function finishProject($id){
         if(Project::where("id", $id)->exists()){
